e23e8c2ac937834224378a5336c280de020f2868
Hugo Drumond <hugo.drumond@fe.up.pt>
2014-10-02 21:11:09 +0100

    Started implementing the sender state machine, but had problem with alarms

--- trab1/src/writenoncanonical.c
+++ trab1/src/writenoncanonical.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <signal.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -12,7 +13,16 @@
 #define FALSE 0
 #define TRUE 1
 
+// Trama
+#define F 0x7e
+#define A 0x03
+#define C 0x03
+
 volatile int STOP=FALSE;
+int flag = 1, tries = 0; // alarm/signal
+
+int controlledWrite( int fd, uint8_t *packetBundle, int nPackets);
+void alarm_handler();
 
 int main(int argc, char** argv)
 {
@@ -20,6 +30,8 @@ int main(int argc, char** argv)
     struct termios oldtio,newtio;
     char buf[255];
     int i, sum = 0, speed = 0;
+    // Trama
+    uint8_t SET[5];
 
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
@@ -27,7 +39,6 @@ int main(int argc, char** argv)
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
-
 
   /*
     Open serial port device for reading and writing and not as controlling tty
@@ -68,15 +79,7 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    printf("Write the information to send\n");
-    fgets(buf, sizeof(buf), stdin);
-    buf[sizeof(buf) - 1] = 0;
-    printf("Size of buffer %d\n", sizeof(buf));
-    printf("Length of string: %d\n", strlen(buf));
-    printf("The message is: %s", buf);
-
-    res = write(fd,buf,strlen(buf)+1);
-    printf("%d bytes written\n", res);
+    controlledWrite(fd,SET,sizeof(SET));
 
     sleep(3);
 
@@ -87,4 +90,25 @@ int main(int argc, char** argv)
 
     close(fd);
     return 0;
+}
+
+int controlledWrite(int fd, uint8_t *packetBundle, int nPackets) {
+
+    int i = 0, t = 0;
+    char Estado = 0;
+
+    if ( (packetBundle == NULL) || ( nPackets == 0) ) return -1;
+
+
+    if ( ( Estado == 0 ) ) {
+        write(fd,SET,nPackets);
+        if ( t == 3 ) return -1;
+        Estado = 1;
+    } else if ( Estado == 1 ) {
+
+
+
+void alarm_handler() {
+    flag = 1;
+    tries++;
 }
